cf83e6674b2447a9223657fd0d483ec2e6d4a26d
EduardoHGomez <hurtado.josegomez@gmail.com>
2024-11-23 21:51:40 -0600

    changes

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware'); 
 console.log('createProxyMiddleware:', createProxyMiddleware);
+
 const morgan = require('morgan'); // Optional: For better logging
 
 const app = express();
